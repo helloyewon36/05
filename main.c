a75685f4bfe0e00741a1ed6c39abad728d61a7b8
helloyewon36 <helloyewon36@gmail.com>
2025-10-02 15:25:28 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,12 +7,11 @@ int main(int argc, char *argv[])
   printf("Input an integer :");
   scanf("%d", &x);
   if (x>0)
-    printf("양수입니다.\n"); 
-  else if (x<0)
-    printf("음수입니다.\n"); 
+    printf("absolute is %i.\n",x);
   else
-    printf("0입니다.\n"); 
-  
+    printf("absolute is %i.\n",-x);
+    
+    
   system("PAUSE");	
   return 0;
 }
